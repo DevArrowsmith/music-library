cafb3cbbf49d06deac2a7d91eff09dfe81730661
devarrowsmith <devarrowsmith@gmail.com>
2021-07-06 19:59:07 +0100

    Update drop-database.js (WIP).

---
+++ utils/drop-database.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql2/promise');
+
+const path = require('path');
+
+const envFile = '../.env.test';
+
+require('dotenv').config({
+    path: path.join(__dirname, envFile),
+});
+
+const dropDatabase = async () => {
+    try {
+        const db = await mysql.createConnection({
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            port: DB_PORT,
+        });
+    await db.query(`DROP DATABASE ${DB_NAME}`);
+    db.close();
+    } catch (err) {
+        console.log('Your environment variables may be wrong. Please double check the .env file.');
+        console.log('Enviornment variables are:', {
+            DB_PASSWORD,
+            DB_NAME,
+            DB_USER,
+            DB_HOST,
+            DB_PORT,
+        });
+        console.log(err);
+    }
+};
+
+dropDatabase();

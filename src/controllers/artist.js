ef3a88f328b81ce78f5c419fca2ba5454cc695ff
devarrowsmith <devarrowsmith@gmail.com>
2021-07-11 18:33:57 +0100

    Create a read route in artist.js that returns all artists in the database. Passes the test artist-read-test.js: returns all artist records in the database.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
@@ -11,6 +12,17 @@ exports.create = async (req, res) => {
         res.sendStatus(201);
     } catch (error) {
         res.sendStatus(500).json(error);
+    }
+    db.close;
+};
+
+exports.read = async (_, res) => {
+    const db = await getDb();
+    try {
+        const [artists] = await db.query('SELECT * FROM Artist');
+        res.status(200).json(artists);
+    } catch (error) {
+        res.status(500).json(error);
     }
     db.close;
 };

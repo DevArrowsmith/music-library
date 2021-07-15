8e7f98a56e74ccd4faddeae205470f4d5b4ed6e8
devarrowsmith <devarrowsmith@gmail.com>
2021-07-15 20:20:22 +0100

    Configure app.js for swagger.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
 const app = express();
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use('/artist', artistRouter);
 app.use('/album', albumRouter);
